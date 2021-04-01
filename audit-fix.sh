44c3dd8dce8016ffdfeba0ddd52b94dde8a40bdb
Eran Medan <eranmeda@amazon.com>
2021-04-01 01:20:14 -0400

    upgrade dependencies

--- audit-fix.sh
+++ audit-fix.sh
@@ -6,13 +6,13 @@ source ./env.sh
 echo "this will perform npm audit --fix on all packages"
 
 echo "folder: lambda/api"
-npm audit --fix --prefix lambda/api
+npm audit fix --prefix lambda/api
 echo "folder: lambda/pretokengeneration"
-npm audit --fix --prefix lambda/pretokengeneration
+npm audit fix --prefix lambda/pretokengeneration
 echo "folder: cdk"
-npm audit --fix --prefix cdk
+npm audit fix --prefix cdk
 echo "folder: ui-react"
-npm audit --fix --prefix ui-react
+npm audit fix --prefix ui-react
 echo "folder: ui-angular"
-npm audit --fix --prefix ui-angular
+npm audit fix --prefix ui-angular
 
